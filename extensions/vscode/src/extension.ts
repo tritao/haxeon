906e8ffb7c6003432d0c69e984e905b57dde7249
Joao Matos <joao@tritao.eu>
2026-09-07 10:37:53 +0100

    Add self-contained VS Code LSP dev workflow

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -10,12 +10,12 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
   const clientOptions: LanguageClientOptions = {documentSelector: [{scheme: "file", language: "haxe"}]};
   client = new LanguageClient("haxeon", "Haxeon", serverOptions, clientOptions);
   context.subscriptions.push(client, vscode.commands.registerCommand("haxeon.profiler.open", async () => {
-    await client!.start();
     ProfilerPanel.show(context, client!);
   }), vscode.commands.registerCommand("haxeon.profiler.setToken", async () => {
     const token = await vscode.window.showInputBox({title: "Haxeon diagnostics token", password: true, ignoreFocusOut: true});
     if (token) await context.secrets.store("haxeon.profiler.token", token);
   }), vscode.commands.registerCommand("haxeon.profiler.clearToken", () => context.secrets.delete("haxeon.profiler.token")));
+  await client.start();
 }
 
 export async function deactivate(): Promise<void> {
