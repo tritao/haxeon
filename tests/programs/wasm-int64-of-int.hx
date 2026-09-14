48f867ed85d74f815a1aa9ef54ba36caf8fce5d0
Joao Matos <joao@tritao.eu>
2026-09-14 22:13:59 +0100

    style: format Wasm Int64 support

--- tests/programs/wasm-int64-of-int.hx
+++ tests/programs/wasm-int64-of-int.hx
@@ -4,6 +4,5 @@ function main():Int {
 		&& haxe.Int64.compare(haxe.Int64.ofInt(1), haxe.Int64.make(0, 1)) == 0
 		&& haxe.Int64.compare(haxe.Int64.ofInt(-1), haxe.Int64.make(-1, -1)) == 0
 		&& haxe.Int64.compare(haxe.Int64.ofInt(2147483647), haxe.Int64.make(0, 2147483647)) == 0
-		&& haxe.Int64.compare(haxe.Int64.ofInt(minimumInt), haxe.Int64.make(-1, minimumInt)) == 0
-		? 42 : 0;
+		&& haxe.Int64.compare(haxe.Int64.ofInt(minimumInt), haxe.Int64.make(-1, minimumInt)) == 0 ? 42 : 0;
 }
